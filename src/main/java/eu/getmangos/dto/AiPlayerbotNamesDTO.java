a8f3c242e7102d94e625069aa7e11690345799e7
Warkdev <cedric.servais@outlook.com>
2020-11-10 23:25:08 +0100

    Add DTO for PlayerbotNames

--- src/main/java/eu/getmangos/dto/AiPlayerbotNamesDTO.java
+++ src/main/java/eu/getmangos/dto/AiPlayerbotNamesDTO.java
@@ -2,6 +2,8 @@ package eu.getmangos.dto;
 
 import java.io.Serializable;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +12,28 @@ import lombok.Setter;
 public class AiPlayerbotNamesDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
-    
+
+    @Schema(description = "An unique identifier for this record")
+    private Integer nameId;
+
+    @Schema(description = "A possible name for a random player bot")
+    private String name;
+
+    @Schema(hidden = true)
+    private Byte gender;
+
+    @Schema(hidden = true)
+    private Short race;
+
+    @Schema(hidden = true)
+    private Short className;
+
+    @Schema(hidden = true)
+    private Integer purpose;
+
+    @Schema(hidden = true)
+    private boolean priority;
+
+    @Schema(hidden = true)
+    private boolean inUse;
 }
