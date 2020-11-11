85cf41f82e996a69ba62f2d5bfc315e8e7ba4e4e
Warkdev <cedric.servais@outlook.com>
2020-11-11 20:33:35 +0100

    Add Rest API interface

---
+++ src/main/java/eu/getmangos/rest/BugreportResource.java
@@ -0,0 +1,106 @@
+package eu.getmangos.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import eu.getmangos.dto.BugReportDTO;
+
+public interface BugreportResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Retrieves a list of bug reports from the database.",
+        description = "This API is returning a list of bug reports from the database. A missing page and page size will return the first 20 results."
+    )
+    @APIResponses(
+        value = {
+            @APIResponse(responseCode = "200", description = "A list of bug reports", content = @Content(
+                        mediaType = "application/json", schema = @Schema(implementation = BugReportDTO.class)
+                )
+            ),
+            @APIResponse(responseCode = "400", description = "Error with the request"),
+            @APIResponse(responseCode = "500", description = "An unexpected event occured")
+        }
+    )
+    public Response findAll(@QueryParam("page") Integer page, @QueryParam("page_size") Integer pageSize);
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Retrieves a bug report from the database.",
+        description = "This API is returning the matching bug report from the database."
+    )
+    @APIResponses(
+        value = {
+            @APIResponse(responseCode = "200", description = "The matching bug report", content = @Content(
+                        mediaType = "application/json", schema = @Schema(implementation = BugReportDTO.class)
+                )
+            ),
+            @APIResponse(responseCode = "400", description = "Error with the request"),
+            @APIResponse(responseCode = "404", description = "Bug report not found"),
+            @APIResponse(responseCode = "500", description = "An unexpected event occured")
+        }
+    )
+    public Response findBugreport(@PathParam("id") int id);
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Creates a bug report within the database.",
+        description = "This API is creating a new bug report within the database."
+    )
+    @APIResponses(
+        value = {
+            @APIResponse(responseCode = "201", description = "The bug report has been created"),
+            @APIResponse(responseCode = "400", description = "Error with the request"),
+            @APIResponse(responseCode = "500", description = "An unexpected event occured")
+        }
+    )
+    public Response createBugreport(BugReportDTO entity);
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Updates a bug report within the database.",
+        description = "This API is updating a new bug report within the database."
+    )
+    @APIResponses(
+        value = {
+            @APIResponse(responseCode = "200", description = "The bug report has been created"),
+            @APIResponse(responseCode = "400", description = "Error with the request"),
+            @APIResponse(responseCode = "500", description = "An unexpected event occured")
+        }
+    )
+    public Response updateBugreport(@PathParam("id") Integer auctionId, BugReportDTO entity);
+
+    @DELETE
+    @Path("{id}")
+    @Operation(summary = "Delete a bug report",
+        description = "This API is deleting an existing bug report based on the provided id."
+    )
+    @APIResponses(
+        value = {
+            @APIResponse(responseCode = "204", description = "The bug report has been deleted", content = @Content(
+                        mediaType = "application/json"
+                )
+            ),
+            @APIResponse(responseCode = "400", description = "Error with the request"),
+            @APIResponse(responseCode = "500", description = "An unexpected even occured")
+        }
+    )
+    public Response deleteBugreport(@PathParam("id") Integer id);
+}
---
+++ src/main/java/eu/getmangos/rest/impl/BugreportResourceService.java
@@ -0,0 +1,112 @@
+package eu.getmangos.rest.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.slf4j.Logger;
+
+import eu.getmangos.controllers.BugReportController;
+import eu.getmangos.controllers.DAOException;
+import eu.getmangos.dto.BugReportDTO;
+import eu.getmangos.entities.Bugreport;
+import eu.getmangos.mapper.BugReportMapper;
+import eu.getmangos.rest.BugreportResource;
+
+@RequestScoped
+@Path("/bug_report/v1")
+@Tag(name = "bug_report")
+public class BugreportResourceService implements BugreportResource {
+
+    @Inject private Logger logger;
+
+    @Inject private BugReportController reportController;
+
+    @Inject private BugReportMapper reportMapper;
+
+    @Override
+    public Response findAll(Integer page, Integer pageSize) {
+        logger.debug("findAll() entry.");
+
+        if(page == null) {
+            page = 1;
+        }
+
+        if(pageSize == null) {
+            pageSize = 20;
+        }
+
+        if(pageSize > 100) {
+            pageSize = 100;
+        }
+
+        List<BugReportDTO> list = new ArrayList<>();
+
+        for(Bugreport report : reportController.findAll(page, pageSize)) {
+            list.add(reportMapper.map(report));
+        }
+
+        logger.debug("findAll() exit.");
+
+        return Response.status(200).entity(list).build();
+    }
+
+    @Override
+    public Response findBugreport(int id) {
+        logger.debug("findBugreport() entry.");
+
+        try {
+            BugReportDTO report = reportMapper.map(reportController.find(id));
+            if(report == null) {
+                return Response.status(404).build();
+            }
+            return Response.status(200).entity(report).build();
+        } catch (DAOException dao) {
+            logger.debug("findBugreport() exit.");
+            return Response.status(500).build();
+        }
+
+    }
+
+    @Override
+    public Response createBugreport(BugReportDTO entity) {
+        try {
+            this.reportController.create(reportMapper.map(entity));
+        } catch (DAOException daoEx) {
+                return Response.status(400).entity(daoEx.getMessage()).build();
+        } catch (Exception ex) {
+                return Response.status(500).entity(ex.getMessage()).build();
+        }
+        return Response.status(201).entity("Bug Report has been created.").build();
+    }
+
+    @Override
+    public Response updateBugreport(Integer auctionId, BugReportDTO entity) {
+        try {
+            this.reportController.update(reportMapper.map(entity));
+        } catch (DAOException daoEx) {
+                return Response.status(400).entity(daoEx.getMessage()).build();
+        } catch (Exception ex) {
+                return Response.status(500).entity(ex.getMessage()).build();
+        }
+        return Response.status(200).entity("Bug Report has been updated.").build();
+    }
+
+    @Override
+    public Response deleteBugreport(Integer id) {
+        try {
+            this.reportController.delete(id);
+        } catch (DAOException daoEx) {
+                return Response.status(400).entity(daoEx.getMessage()).build();
+        } catch (Exception ex) {
+                return Response.status(500).entity(ex.getMessage()).build();
+        }
+        return Response.status(204).build();
+    }
+
+}
