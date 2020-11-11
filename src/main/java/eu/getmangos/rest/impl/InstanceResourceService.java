c78e25d5fea722928e0d57bfbe31f59bc0538dd2
Warkdev <cedric.servais@outlook.com>
2020-11-11 21:54:28 +0100

    Add initial InstanceResource implementation

---
+++ src/main/java/eu/getmangos/rest/impl/InstanceResourceService.java
@@ -0,0 +1,102 @@
+package eu.getmangos.rest.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.slf4j.Logger;
+
+import eu.getmangos.controllers.CreatureRespawnController;
+import eu.getmangos.controllers.DAOException;
+import eu.getmangos.dto.CreatureRespawnDTO;
+import eu.getmangos.entities.CreatureRespawn;
+import eu.getmangos.mapper.CreatureRespawnMapper;
+import eu.getmangos.rest.InstanceResource;
+
+@RequestScoped
+@Path("/instance/v1")
+@Tag(name = "instance")
+public class InstanceResourceService implements InstanceResource {
+
+    @Inject private Logger logger;
+
+    @Inject private CreatureRespawnController creatureRespawnController;
+
+    @Inject private CreatureRespawnMapper creatureMapper;
+
+    @Override
+    public Response findAllCreatureRespawn(Integer instanceId, Integer page, Integer pageSize) {
+        logger.debug("findAllCreatureRespawn() entry.");
+
+        if(page == null) {
+            page = 1;
+        }
+
+        if(pageSize == null) {
+            pageSize = 20;
+        }
+
+        if(pageSize > 100) {
+            pageSize = 100;
+        }
+
+        List<CreatureRespawnDTO> list = new ArrayList<>();
+
+        try{
+            for(CreatureRespawn respawn : creatureRespawnController.findAllByInstance(instanceId, page, pageSize)) {
+                list.add(creatureMapper.map(respawn));
+            }
+        } catch (DAOException dao) {
+            return Response.status(404).entity(list).build();
+        }
+
+        logger.debug("findAllCreatureRespawn() exit.");
+
+        return Response.status(200).entity(list).build();
+    }
+
+    @Override
+    public Response createCreatureRespawn(Integer instanceId, CreatureRespawnDTO entity) {
+        try {
+            entity.setInstance(instanceId);
+            this.creatureRespawnController.create(creatureMapper.map(entity));
+        } catch (DAOException daoEx) {
+                return Response.status(400).entity(daoEx.getMessage()).build();
+        } catch (Exception ex) {
+                return Response.status(500).entity(ex.getMessage()).build();
+        }
+        return Response.status(201).entity("Respawn Timer has been created.").build();
+    }
+
+    @Override
+    public Response updateCreatureRespawnTimer(Integer instanceId, Integer guid, CreatureRespawnDTO entity) {
+        try {
+            entity.setGuid(guid);
+            entity.setInstance(instanceId);
+            this.creatureRespawnController.update(creatureMapper.map(entity));
+        } catch (DAOException daoEx) {
+                return Response.status(400).entity(daoEx.getMessage()).build();
+        } catch (Exception ex) {
+                return Response.status(500).entity(ex.getMessage()).build();
+        }
+        return Response.status(200).entity("Respawn Timer has been updated.").build();
+    }
+
+    @Override
+    public Response deleteCreatureRespawnTimer(Integer instanceId, Integer guid) {
+        try {
+            this.creatureRespawnController.delete(instanceId, guid);
+        } catch (DAOException daoEx) {
+                return Response.status(400).entity(daoEx.getMessage()).build();
+        } catch (Exception ex) {
+                return Response.status(500).entity(ex.getMessage()).build();
+        }
+        return Response.status(204).build();
+    }
+
+}
